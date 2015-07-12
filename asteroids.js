2170ffe701a795ee39eb4b3a89310c867fe9e2ee
davis <dbh937@gmail.com>
2015-07-12 02:10:31 -0400

    moved point and vector classes to a separate file

--- asteroids.js
+++ asteroids.js
@@ -5,69 +5,6 @@ const ROTATION_RATE = 25;
 var Ship = new Sprite([new Point(-10, -12), new Point(0, 20), new Point(10, -12)], new Point(canvas.width/2, canvas.height/2));
 var Thruster = new Sprite([new Point(-7, -15), new Point(0, -23), new Point(7, -15)], new Point(Ship.coords.x, Ship.coords.y));
 
-function Point(x, y) {
-  this.x = Math.round(x*10)/10;
-  this.y = Math.round(y*10)/10;
-}
-
-Point.rotation = function(p, theta) {
-  return new Point(Math.cos(theta)*p.x + Math.sin(theta)*p.y, 
-                  -Math.sin(theta)*p.x + Math.cos(theta)*p.y);
-}
-
-Point.add = function(a, b) {
-  return new Point(a.x + b.x, a.y + b.y);
-}
-
-
-function Vector(angle, magnitude) {
-  this.theta = angle;
-  this.magnitude = magnitude;
-}
-
-Vector.prototype.components = function() {
-  // basically make the magnitude point straight up and rotate it by theta to get x and y.
-  var m = new Point(0, this.magnitude);
-  // console.log(m);
-  var p = Point.rotation(m, this.theta);
-  // console.log(p)
-  return p;
-}
-
-Vector.add = function(a, b) {
-  var a1 = a.components();
-  var b1 = b.components();
-  
-  var sum = Point.add(a1, b1);
-  var theta = Math.atan(sum.y/sum.x); // angle of vector is asin of slope
-
-  if (sum.x == 0) // can't divide by 0, but we still want the vector
-    theta = Math.PI/2;
-  if (sum.y == 0) {
-    theta = Math.PI/2 * (sum.x > 0 ? 1 : -1);
-  }
-  if (sum.y < 0)
-    theta = (theta + Math.PI) % (Math.PI * 2);
-
-  var magnitude = Math.sqrt(Math.pow(sum.x, 2) + Math.pow(sum.y, 2)); // dist from origin
-  var v = new Vector(theta, magnitude);
-  // console.log("VELOCITY: ");
-  // console.log(a1);
-  // console.log("ACCELERATION: ");
-  // console.log(b1);
-  // console.log("SUM:");
-  // console.log(sum);
-  // console.log("THETA:");
-  // console.log(theta);
-  // console.log("MAGNITUDE:");
-  // console.log(magnitude);
-  // console.log("VECTOR:");
-  // console.log(v);
-  // console.log("\n");
-  return v;
-}
-
-
 function Sprite(pts, start) {
   this.vertices = pts; // array of Points that define the polygon of the sprite.
   this.coords = start; // point of the center of the Sprite
@@ -81,7 +18,8 @@ Sprite.prototype.draw = function(ctx) {
   for (var i=0; i < this.vertices.length; i++) {
     rotation.push(Point.rotation(this.vertices[i], this.theta));
   }
-
+  
+  // wraparound code
   if (this.coords.x > canvas.width)
     this.coords.x = 0;
   else if (this.coords.x < 0)
@@ -110,17 +48,12 @@ Sprite.prototype.move = function() {
   /*
     Add velocity vector to the current location.
   */
-  // console.log(this.theta);
-  // console.log(this.velocity.theta);
   this.coords = Point.add(this.coords, this.velocity.components());
   if (this.velocity.magnitude > 15)
     this.velocity.magnitude = 15;
 }
 
-
-
 window.onkeydown = function(e) {
-  // ctx.clearRect(0,0,1000,1000);
   if (e.which == 37) {
     Ship.rotation = "left";
   }
@@ -149,11 +82,7 @@ function step() {
     Ship.rotate(-Math.PI/ROTATION_RATE);
 
   if (Ship.thruster === "on") {
-    // Ship.moveForward(5);
-    // console.log(Vector.add(Ship.velocity, new Vector(Ship.theta, 1)));
-    // console.log(Ship.theta);
     var v = new Vector(Ship.theta, 1);
-    // console.log(v);
     Ship.velocity = Vector.add(Ship.velocity, v);
     Thruster.draw(ctx);
   }
